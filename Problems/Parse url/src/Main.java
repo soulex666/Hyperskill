202323ec611738fa2ac44eb9c8e630be024a3b75
Vladimir <soulexmail@gmail.com>
2020-10-16 00:55:33 +0300

    Add some text of the problem

--- Problems/Parse url/src/Main.java
+++ Problems/Parse url/src/Main.java
@@ -1,7 +1,34 @@
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
+
+/*Parse url
+        You want to hack a website now. First, get all the available parameters that you can find in the URL. Then print them in the "key : value" format. If a parameter doesn't have value, print "not found".
+
+        If you find the password (parameter pass), you should print its value after all parameters once again, but with a key password. If a URL does not contain parameter pass, do not print anything after the listed parameters. However, if pass parameter is present, its value cannot be empty.
+
+        Note: the order of parameters should be the same as in the URL.
+        Advice: Check examples for better understanding and carefully learn the structure of the URL.
+
+
+        Sample Input:
+        https://target.com/index.html?pass=12345&port=8080&cookie=&host=localhost
+
+        Sample Output:
+        pass : 12345
+        port : 8080
+        cookie : not found
+        host : localhost
+        password : 12345
+
+
+        Sample Input:
+        https://target.com/index.html?port=8080&cookie=&host=localhost
+
+        Sample Output:
+        port : 8080
+        cookie : not found
+        host : localhost*/
 
 class Main {
     public static void main(String[] args) {
